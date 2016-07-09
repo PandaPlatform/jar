8b1dc9539e660c1fb796abe415bb16dfe2bf9df3
Ioannis Papikas <papikas.ioan@gmail.com>
2016-07-09 14:03:14 +0200

    Header comments.

--- ServerReport.php
+++ ServerReport.php
@@ -1,8 +1,15 @@
 <?php
 
-namespace Panda\Jar;
+/*
+ * This file is part of the Panda framework Jar component.
+ *
+ * (c) Ioannis Papikas <papikas.ioan@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
-use Symfony\Component\HttpFoundation\Response;
+namespace Panda\Jar;
 
 /**
  * Server Report Protocol
