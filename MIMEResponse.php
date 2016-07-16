4c31eb6d6b4e8873542b68d9b5515eb453af621e
Ioannis Papikas <papikas.ioan@gmail.com>
2016-07-16 22:50:39 +0200

    MIMEResponse updated to allow attachments and inline files.

--- MIMEResponse.php
+++ MIMEResponse.php
@@ -22,24 +22,57 @@ namespace Panda\Jar;
 class MIMEResponse extends AsyncResponse
 {
     /**
-     * Set the file to send/download.
+     * Set the file to be sent.
      *
-     * @param mixed  $fileContents
+     * @param string $file
      * @param string $type
      * @param string $suggestedFileName
+     * @param string $disposition
      * @param bool   $ignore_user_abort
      */
-    public function setFile($fileContents, $type = self::CONTENT_APP_STREAM, $suggestedFileName = '', $ignore_user_abort = false)
+    public function setFile($file, $type = self::CONTENT_APP_STREAM, $suggestedFileName = '', $disposition = 'attachment', $ignore_user_abort = false)
     {
-        // Set Response Headers
-        $this->headers->set('Content-Type', $type);
-        $this->headers->set('Content-Disposition', 'attachment; filename=' . $suggestedFileName);
-        $this->headers->set('Content-Length', filesize($fileContents));
+        // Set response headers
+        $this->setHeaders($type, $suggestedFileName, $disposition, $ignore_user_abort);
 
-        // Set buffer settings
-        ignore_user_abort($ignore_user_abort);
+        // Set response content
+        $this->content = file_get_contents($file);
+    }
+
+    /**
+     * Set the file contents to be sent.
+     *
+     * @param string $fileContents
+     * @param string $type
+     * @param string $suggestedFileName
+     * @param string $disposition
+     * @param bool   $ignore_user_abort
+     */
+    public function setFileContents($fileContents, $type = self::CONTENT_APP_STREAM, $suggestedFileName = '', $disposition = 'attachment', $ignore_user_abort = false)
+    {
+        // Set response headers
+        $this->setHeaders($type, $suggestedFileName, $disposition, $ignore_user_abort);
 
         // Set response content
         $this->content = $fileContents;
+    }
+
+    /**
+     * Set the response headers.
+     *
+     * @param string $type
+     * @param string $suggestedFileName
+     * @param string $disposition
+     * @param bool   $ignore_user_abort
+     */
+    private function setHeaders($type = self::CONTENT_APP_STREAM, $suggestedFileName = '', $disposition = 'attachment', $ignore_user_abort = false)
+    {
+        // Set Response Headers
+        $this->headers->set('Content-Type', $type);
+        $disposition = ($disposition ?: 'attachment');
+        $this->headers->set('Content-Disposition', $disposition . '; filename=' . $suggestedFileName);
+
+        // Set buffer settings
+        ignore_user_abort($ignore_user_abort);
     }
 }
